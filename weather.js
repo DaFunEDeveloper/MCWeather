0350779930c4b278d380928a73bae0dbc581bb2b
DaFunEDeveloper <62800011+DaFunEDeveloper@users.noreply.github.com>
2020-04-15 09:14:36 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,8 @@
 "use strict"
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=rochester+hills&appid=ad0ad46e12935e5d07614976f2b10ef1"
+var place = prompt("What is Your Zipcode?")
+
+var url = "https://api.openweathermap.org/data/2.5/weather?zip=" + place + ",us&appid=ad0ad46e12935e5d07614976f2b10ef1"
 
 $.ajax ({
 	url: url,
